2c056a6c8f55a296c896977782b919dbd58a7ac3
Max Heckel <maxheckel@maxs-mbp.lan>
2023-01-02 11:23:44 -0500

    Analytics

--- static/src/main.js
+++ static/src/main.js
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import VueGtag from "vue-gtag";
 import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(VueGtag,  {
+    config: { id: "G-K726D0Q3X5" }
+})
 
 app.mount('#app')
